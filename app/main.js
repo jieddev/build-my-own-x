5504b2f12441bb2d6dc596c97527be5526b16355
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-07 23:08:53 +0800

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,18 @@
 const readline = require("readline");
+const { start } = require("repl");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-// TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (command) => {
-  console.log(`${command}: command not found`);
-  // rl.close();
-});
+function startTerminal() {
+  rl.question("$ ", (command) => {
+    console.log(`${command}: command not found`);
+    // rl.close();
+    startTerminal();
+  });
+
+}
+
+startTerminal();
