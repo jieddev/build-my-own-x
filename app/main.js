c8ef06b13189bfe41c3405f8a4034d9a24deb605
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-08 23:15:00 +0800

    Add if condition to check exit command to close terminal

--- app/main.js
+++ app/main.js
@@ -8,6 +8,10 @@ const rl = readline.createInterface({
 
 function startTerminal() {
   rl.question("$ ", (command) => {
+    if(command == "exit") {
+      rl.close();
+    }
+  
     console.log(`${command}: command not found`);
     // rl.close();
     startTerminal();
